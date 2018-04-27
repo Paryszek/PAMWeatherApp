91ca735fe340202b506d1d430ebe420aac222f17
Paryszek <michal.parysz@yahoo.com>
2018-04-27 15:21:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/michalparysz/weatherapp/MoonFragment.java
@@ -0,0 +1,13 @@
+package com.example.michalparysz.weatherapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by michalparysz on 27.04.2018.
+ */
+
+public class MoonFragment extends Fragment {
+    public MoonFragment() {
+        // Required empty public constructor
+    }
+}
---
+++ app/src/main/java/com/example/michalparysz/weatherapp/SunFragment.java
@@ -0,0 +1,13 @@
+package com.example.michalparysz.weatherapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by michalparysz on 27.04.2018.
+ */
+
+public class SunFragment extends Fragment {
+    public SunFragment() {
+        // Required empty public constructor
+    }
+}
