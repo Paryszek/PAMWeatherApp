b98b68448be96712abc977ae253ae1ec3c83ec8a
Michal Parysz <michal.parysz@r.pl>
2018-05-16 19:49:37 +0200

    View done, additional astroweather library included

--- app/src/main/java/com/example/michalparysz/weatherapp/MoonFragment.java
+++ app/src/main/java/com/example/michalparysz/weatherapp/MoonFragment.java
@@ -1,13 +1,32 @@
 package com.example.michalparysz.weatherapp;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.astrocalculator.AstroCalculator;
+import com.astrocalculator.AstroDateTime;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by michalparysz on 27.04.2018.
  */
 
+
 public class MoonFragment extends Fragment {
+    @BindView(R.id.moonRise)
+    TextView moonRise;
     public MoonFragment() {
-        // Required empty public constructor
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AstroCalculator astroCalculator = new AstroCalculator(new AstroDateTime(), new AstroCalculator.Location(50, 50));
+        moonRise.setText(astroCalculator.getMoonInfo().getMoonrise().toString());
     }
 }
--- app/src/main/java/com/example/michalparysz/weatherapp/SunFragment.java
+++ app/src/main/java/com/example/michalparysz/weatherapp/SunFragment.java
@@ -1,6 +1,7 @@
 package com.example.michalparysz.weatherapp;
 
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by michalparysz on 27.04.2018.
