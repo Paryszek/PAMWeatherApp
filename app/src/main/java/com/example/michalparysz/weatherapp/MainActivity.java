ae878ac4b0cc921b2b627dd60f900006eda5d273
Paryszek <michal.parysz@yahoo.com>
2018-05-17 23:41:12 +0200

    Timer and period refresh

--- app/src/main/java/com/example/michalparysz/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/michalparysz/weatherapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.michalparysz.weatherapp;
 
+import android.annotation.SuppressLint;
+import android.os.CountDownTimer;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,7 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.astrocalculator.AstroCalculator;
 import com.astrocalculator.AstroDateTime;
@@ -16,19 +20,30 @@ import com.astrocalculator.AstroDateTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     private static AstroCalculator astroCalculator;
     private FragmentAdapter _fragmentAdapter;
     private ViewPager viewPager;
+    private int _latitude;
+    private int _longitude;
+    private int _refreshPeriod;
+
     @BindView(R.id.latitudeInput)
     TextInputLayout latiduteInput;
     @BindView(R.id.longitudeInput)
     TextInputLayout longitudeInput;
+    @BindView(R.id.refreshInput)
+    TextInputLayout refreshInput;
+
+    @BindView(R.id.clock)
+    TextView clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,34 @@ public class MainActivity extends AppCompatActivity{
         viewPager = findViewById(R.id.container);
         setupViewPager(viewPager);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Timer updateData = new Timer();
+        updateData.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        reloadView();
+                    }
+                });
+            }
+        }, 0, _refreshPeriod);
+        Timer updateTime = new Timer();
+        updateTime.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        updateClock();
+                    }
+                });
+            }
+        },0, 1000);
+    }
+
     public AstroCalculator getAstro() {
         return astroCalculator;
     }
@@ -49,7 +92,6 @@ public class MainActivity extends AppCompatActivity{
     @OnClick({R.id.moonButton})
     public void onClickMoonButton(View view) {
         setViewPager(0);
-
     }
 
     @OnClick({R.id.sunButton})
@@ -59,9 +101,44 @@ public class MainActivity extends AppCompatActivity{
 
     @OnClick(R.id.zatwierdz)
     public void OnClickZatwierdz() {
-        int latitudeValue = Integer.parseInt(Objects.requireNonNull(latiduteInput.getEditText()).getText().toString());
-        int longitudeValue = Integer.parseInt(Objects.requireNonNull(longitudeInput.getEditText()).getText().toString());
-        astroCalculator = new AstroCalculator(getDate(), new AstroCalculator.Location(latitudeValue, longitudeValue));
+        try {
+            int latV = Integer.parseInt(Objects.requireNonNull(latiduteInput.getEditText()).getText().toString());
+            int longV = Integer.parseInt(Objects.requireNonNull(longitudeInput.getEditText()).getText().toString());
+            int refreshV = Integer.parseInt(Objects.requireNonNull(refreshInput.getEditText()).getText().toString());
+            if (validation(latV, "latidute") && validation(longV, "longitude") && validation(refreshV, "refreshPeriod")) {
+                _latitude = latV;
+                _longitude = longV;
+                _refreshPeriod = refreshV;
+                reloadView();
+            } else {
+                Toast.makeText(this, "Wrong inputs, please try again", Toast.LENGTH_LONG).show();
+            }
+        } catch (NumberFormatException e) {}
+    }
+    @SuppressLint("SetTextI18n")
+    private void updateClock() {
+        Date astroDateTime = new Date();
+        String _hours = addZero(((Integer) astroDateTime.getHours()).toString());
+        String _minutes = addZero(((Integer) astroDateTime.getMinutes()).toString());
+        String _seconds = addZero(((Integer) astroDateTime.getSeconds()).toString());
+        clock.setText(_hours + ":" + _minutes + ":" + _seconds);
+    }
+    private boolean validation(int value, String type) {
+        switch (type) {
+            case "latidute":
+                if (-90 <= value && value <= 90) return true;
+                break;
+            case "longitude":
+                if (-180 <= value && value <= 180) return true;
+                break;
+            case "refreshPeriod":
+                if (value >= 0) return true;
+                break;
+        }
+        return false;
+    }
+    private void reloadView() {
+        astroCalculator = new AstroCalculator(getDate(), new AstroCalculator.Location(_latitude, _longitude));
         MoonFragment moonFragment = (MoonFragment) _fragmentAdapter.getItem(0);
         SunFragment sunFragment = (SunFragment) _fragmentAdapter.getItem(1);
         moonFragment.reloadMoonFragment();
@@ -73,9 +150,12 @@ public class MainActivity extends AppCompatActivity{
         _fragmentAdapter.addFragment(new SunFragment(), "Sun");
         viewPager.setAdapter(_fragmentAdapter);
     }
+    @SuppressLint("SetTextI18n")
     private void initAstro() {
+        _refreshPeriod = 10000;
         Objects.requireNonNull(latiduteInput.getEditText()).setText("0");
         Objects.requireNonNull(longitudeInput.getEditText()).setText("0");
+        Objects.requireNonNull(refreshInput.getEditText()).setText(((Integer) _refreshPeriod).toString());
         int latitudeValue = Integer.parseInt(Objects.requireNonNull(latiduteInput.getEditText()).getText().toString());
         int longitudeValue = Integer.parseInt(Objects.requireNonNull(latiduteInput.getEditText()).getText().toString());
         astroCalculator = new AstroCalculator(getDate(), new AstroCalculator.Location(latitudeValue, longitudeValue));
@@ -92,4 +172,9 @@ public class MainActivity extends AppCompatActivity{
         int timezoneOffset =  Calendar.getInstance().get(Calendar.ZONE_OFFSET) / 3600000;
         return new AstroDateTime(year, month, day, hour, minute, second, timezoneOffset, true);
     }
+
+    private String addZero(String value) {
+        return value.length() == 1 ? "0" + value : value;
+    }
+
 }
