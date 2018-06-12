f8e3a40b82b2ae7713c6dc5146d6fee117693053
Paryszek <michal.parysz@yahoo.com>
2018-06-12 09:40:59 +0200

    Model classes for weather api

---
+++ app/src/main/java/com/example/michalparysz/weatherapp/Models/Forecast/Forecast.java
@@ -0,0 +1,22 @@
+package com.example.michalparysz.weatherapp.Models.Forecast;
+
+public class Forecast
+{
+    private Forecastday[] forecastday;
+
+    public Forecastday[] getForecastday ()
+    {
+        return forecastday;
+    }
+
+    public void setForecastday (Forecastday[] forecastday)
+    {
+        this.forecastday = forecastday;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [forecastday = "+forecastday+"]";
+    }
+}
